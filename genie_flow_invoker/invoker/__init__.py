5dbdd73bbdab0630267b5bb45ef310ea01b7ec99
Willem van Asperen <willem@van.asperen.org>
2024-09-23 16:09:36 +0200

    moving out the OpenAI stuff

--- genie_flow_invoker/invoker/__init__.py
+++ genie_flow_invoker/invoker/__init__.py
@@ -1,5 +1,4 @@
 from .api import APIInvoker
 from .neo4j import Neo4jInvoker
-from .openai import AzureOpenAIChatInvoker, AzureOpenAIChatJSONInvoker
 from .verbatim import VerbatimInvoker
 from .weaviate import WeaviateSimilaritySearchInvoker
